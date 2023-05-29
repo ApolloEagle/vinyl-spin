315f64b5a7effc91af12ec699c9b157c1bdd97a0
Blake Reimer <sobeksea@gmail.com>
2023-05-29 23:57:40 +0000

    handle screen sharing permission

--- src/helpers/record-video.ts
+++ src/helpers/record-video.ts
@@ -1,36 +1,39 @@
 const recordVideo = async (duration: number) => {
-  const mediaStream: MediaStream = await navigator.mediaDevices.getDisplayMedia(
-    {
-      video: true,
-    }
-  );
+  try {
+    const mediaStream: MediaStream =
+      await navigator.mediaDevices.getDisplayMedia({
+        video: true,
+      });
 
-  const mediaRecorder: MediaRecorder = new MediaRecorder(mediaStream, {
-    mimeType: "video/webm",
-  });
-
-  const recordedChunks: Blob[] = [];
-  mediaRecorder.addEventListener("dataavailable", (e) => {
-    recordedChunks.push(e.data);
-  });
-
-  mediaRecorder.addEventListener("stop", function () {
-    const blob = new Blob(recordedChunks, {
-      type: "video/webm",
+    const mediaRecorder: MediaRecorder = new MediaRecorder(mediaStream, {
+      mimeType: "video/webm",
     });
 
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = "video.webm";
-    a.click();
-  });
+    const recordedChunks: Blob[] = [];
+    mediaRecorder.addEventListener("dataavailable", (e) => {
+      recordedChunks.push(e.data);
+    });
 
-  setTimeout(() => {
-    mediaRecorder.stop();
-  }, duration * 1000);
+    mediaRecorder.addEventListener("stop", function () {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
 
-  mediaRecorder.start();
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "video.webm";
+      a.click();
+    });
+
+    setTimeout(() => {
+      mediaRecorder.stop();
+    }, duration * 1000);
+
+    mediaRecorder.start();
+  } catch (error) {
+    console.log("Denied bruv: ", error);
+  }
 };
 
 export default recordVideo;
